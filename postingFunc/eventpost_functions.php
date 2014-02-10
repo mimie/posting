6d0df1aa2e3af416d5cf55789bd2faff8d9c1665
mimie <karen.convicto@yahoo.com>
2014-02-10 12:01:23 +0800

    added displayEventBillings function

--- postingFunc/eventpost_functions.php
+++ postingFunc/eventpost_functions.php
@@ -13,4 +13,53 @@
    
   }
 
+  function displayEventBillings(array $eventBillings){
+
+    $html = "<table>"
+          . "<thead>"
+          . "<tr>"
+          . "<th>Select Contact</th>"
+          . "<th>Participant Id</th>"
+          . "<th>Event Type</th>"
+          . "<th>Event Name</th>"
+          . "<th>Participant Name</th>"
+          . "<th>Organization Name</th>"
+          . "<th>Fee Amount</th>"
+          . "<th>Billing Number</th>"
+          . "<th>Billing Date</th>"
+          . "</tr>"
+          . "</thead>";
+
+    $html = $html."<tbody>";
+
+    foreach($eventBillings as $key => $field){
+
+       $contactId = $field["contact_id"];
+       $participantId = $field["participant_id"];
+       $eventType = $field["event_type"];
+       $eventName = $field["event_name"];
+       $name = $field["participant_name"];
+       $orgName = $field["organization_name"];
+       $feeAmount = $field["fee_amount"];
+       $billingNo = $field["billing_no"];
+       $date = $field["bill_date"];
+
+       $html = $html."<tr>"
+             . "<td><input type='checkbox' name='contactIds[]' value='$contactId'></td>"
+             . "<td>$participantId</td>"
+             . "<td>$eventType</td>"
+             . "<td>$eventName</td>"
+             . "<td>$name</td>"
+             . "<td>$orgName</td>"
+             . "<td>$feeAmount</td>"
+             . "<td>$billingNo</td>"
+             . "<td>$date</td>"
+             . "</tr>";
+    }
+  
+    $html = $html."</tbody></table>";
+    return $html;
+
+}
+
 ?>

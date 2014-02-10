ec34359794c9d880e7c4a0e58264e1cb3ee6dbf8
mimie <karen.convicto@yahoo.com>
2014-02-10 14:03:19 +0800

    added menus

--- eventCompanyPosting.php
+++ eventCompanyPosting.php
@@ -1,2 +1,27 @@
+<html>
+<head>
+  <title>Company Event Posting</title>
+  <link rel="stylesheet" type="text/css" href="billingStyle.css">
+  <link rel="stylesheet" type="text/css" href="menu.css">
+</head>
+<body>
 <?php
+
+  include "login_functions.php";
+  include "pdo_conn.php";
+  include "postingFunc/eventpost_functions.php";
+
+  $dbh = civicrmConnect();
+  $menu = logoutDiv($dbh);
+
+  echo $menu;
+  echo "<br>";
+   echo "<table width='100%'>"
+        . "<tr>"
+        . "<td  bgcolor='#084B8A'><a href='eventIndividualPosting.php'>INDIVIDUAL EVENT POSTING</a></td>"
+        . "<td><a href='eventCompanyPosting.php'>COMPANY EVENT POSTING</a></td>"
+        . "</tr>"
+        . "</table><br><br>";
 ?>
+</body>
+</html>

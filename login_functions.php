5ce883fd7c2f39f24da3a985e7b0516a3e700577
mimie <karen.convicto@yahoo.com>
2014-02-10 12:18:53 +0800

    change link of event posting

--- login_functions.php
+++ login_functions.php
@@ -112,7 +112,7 @@ function logoutDiv($dbh){
 
      $html = "<div width='100%' style='background-color:black; padding:1px;'>"
            . "<ul>"
-           . "<li><a href='events2.php?'>Event</a><ul><li><a href='eventPosting.php'>Event Posting</a></li></ul></li>"
+           . "<li><a href='events2.php?'>Event</a><ul><li><a href='eventIndividualPosting.php'>Event Posting</a></li></ul></li>"
            . "<li><a href='#'>Membership</a>"
            . "<ul><li><a href='membershipIndividualBilling.php'>Membership Billing</a></li></ul>"
            . "</li>"

72b6a8b3158a9d357532aa628de9193e41c6ea21
mimie <karen.convicto@yahoo.com>
2014-02-10 12:18:19 +0800

    rename file eventPosting to eventIndividual Posting

---
+++ eventIndividualPosting.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+ <title>Event Posting</title>
+ <link rel="stylesheet" type="text/css" href="billingStyle.css">
+ <link rel="stylesheet" type="text/css" href="menu.css">
+</head>
+<body>
+<?php
+   include "login_functions.php";
+   include "pdo_conn.php";
+   include "postingFunc/eventpost_functions.php";
+
+
+   $dbh = civicrmConnect();
+   $menu = logoutDiv($dbh);
+
+   echo $menu;
+   echo "<br>";
+
+   echo "<table width='100%'>"
+        . "<tr>"
+        . "<td>INDIVIDUAL EVENT POSTING</td>"
+        . "<td bgcolor='#084B8A'>COMPANY EVENT POSTING</td>"
+        . "</tr>"
+        . "</table><br><br>";
+
+   $eventBillings = getGeneratedEventBillings($dbh);
+   $display = displayEventBillings($eventBillings);
+   echo $display;
+?>
+</body>
+</html>

7032aa2d84c58ca21d9af0a363defeaab44bf613
mimie <karen.convicto@yahoo.com>
2014-02-10 11:06:00 +0800

    fix sql query FROM table

--- postingFunc/eventpost_functions.php
+++ postingFunc/eventpost_functions.php
@@ -1,9 +1,10 @@
 <?php
 
-  function getGeneratedEventBilling($dbh){
+  function getGeneratedEventBillings(PDO $dbh){
 
    $sql = $dbh->prepare("SELECT contact_id, participant_id, event_type, event_name, participant_name,
                          organization_name, org_contact_id, fee_amount, billing_no, bill_date
+                         FROM billing_details
                          WHERE billing_type = 'Individual' AND post_bill='0'");
    $sql->execute();
    $result = $sql->fetchAll(PDO::FETCH_ASSOC);

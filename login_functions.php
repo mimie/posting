3d1dce0afc6f33508939a6abcbb71f16dfb8ae55
mimie <karen@imperium.ph>
2014-03-07 17:55:08 +0800

    added new link to membership billing

--- login_functions.php
+++ login_functions.php
@@ -114,7 +114,7 @@ function logoutDiv($dbh){
            . "<ul>"
            . "<li><a href='events2.php?'>Event</a><ul><li><a href='eventIndividualPosting.php'>Event Posting</a></li></ul></li>"
            . "<li><a href='#'>Membership</a>"
-           . "<ul><li><a href='membershipIndividualBilling.php'>Membership Billing</a></li></ul>"
+           . "<ul><li><a href='membershipBillingView.php'>Membership Billing</a></li></ul>"
            . "</li>"
            . "<li><a href='addCustomer.php'>Civicrm Contacts</a></li>"
            . "<li><a href='logout.php'>Logout</a></li>"

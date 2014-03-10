b8247c2fc46e85d98d7ce5ebe0251c3895b80c32
mimie <karen@imperium.ph>
2014-03-10 16:25:24 +0800

    added function searchCompanyPostedBillings

--- postingFunc/companyViewPost_functions.php
+++ postingFunc/companyViewPost_functions.php
@@ -68,4 +68,35 @@ function displayCompanyPostedBillings(array $billingDetails){
   return $html;
 }
 
+function searchCompanyPostedBillings($dbh,$searchType,$searchValue){
+
+  $searchQuery = '';
+  
+  switch($searchType){
+
+    case 'org_name':
+         $searchQuery = "AND cc.display_name LIKE ? AND cc.contact_type = 'Organization'";
+         break;
+    case 'event_name':
+         $searchQuery = "AND ce.title LIKE ?";
+         break;
+    case 'billing_no':
+         $searchQuery = "AND bc.billing_no LIKE ?";
+         break;
+  }
+  $sql = $dbh->prepare("SELECT bc.event_id, ce.title as event_name, bc.org_contact_id, 
+                        cc.display_name as organization_name, bc.billing_no, 
+                        bc.total_amount, bc.subtotal,bc.vat, bc.bill_date
+                        FROM billing_company bc, civicrm_event ce, civicrm_contact cc
+                        WHERE bc.event_id = ce.id
+                        AND bc.org_contact_id = cc.id
+                        AND post_bill = '1'
+                        AND total_amount != '0'
+                        $searchQuery");
+  $sql->bindValue(1,"%".$searchValue."%",PDO::PARAM_STR);
+  $sql->execute();
+  $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+  return $result;
+}
 ?>

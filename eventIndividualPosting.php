e575362824e8e1de3b7bbe477dddbbee0b4601db
mimie <karen@imperium.ph>
2014-03-25 16:11:56 +0800

    added tab for participant list

--- eventIndividualPosting.php
+++ eventIndividualPosting.php
@@ -53,6 +53,7 @@ $(function() {
 
    echo "<table width='100%'>"
         . "<tr>"
+        . "<td bgcolor='#084B8A'><a href='events2.php'>PARTICIPANT LIST</a></td>"
         . "<td><a href='eventIndividualPosting.php'>INDIVIDUAL EVENT POSTING</a></td>"
         . "<td bgcolor='#084B8A'><a href='eventCompanyPosting.php'>COMPANY EVENT POSTING</a></td>"
         . "</tr>"

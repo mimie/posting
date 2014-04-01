c1b503014760d525eeaa78e3653e24928d536d94
mimie <karen.convicto@yahoo.com>
2014-04-01 17:18:13 +0800

    added script for checking all checkbox

--- EventCompanyPostingByEvent.php
+++ EventCompanyPostingByEvent.php
@@ -3,6 +3,41 @@
  <title>Company Billing</title>
  <link rel="stylesheet" type="text/css" href="billingStyle.css">
  <link rel="stylesheet" type="text/css" href="menu.css">
+ <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script src="js/jquery-jPaginate.js"></script>
+  <script src="js/jquery.tablesorter.js"></script>
+<script>
+$(function() {
+        $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+        $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+        $('#billings').jPaginate({
+                'max': 10,
+                'page': 1,
+                'links': 'buttons'
+        });
+//        $("table").tablesorter( {sortList: [[0,0], [1,0]]} ); 
+});
+$(function() {
+    $( "#confirmation" ).dialog({
+      resizable: false,
+      width:500,
+      modal: true,
+      buttons: {
+        "OK": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  });
+
+$(function() {
+    $( "#datepickerStart" ).datepicker();
+    $( "#datepickerEnd" ).datepicker();
+    $( "#postDate" ).datepicker();
+});
+</script>
 </head>
 <body>
 <?php
@@ -66,4 +101,16 @@
 
 ?>
 </body>
+<script type="text/javascript">                                                                
+  $("#check").click(function(){                                                                
+          
+    if($(this).is(":checked")){                                                                
+      $("body input[type=checkbox][class=checkbox]").prop("checked",true);                     
+    }else{
+      $("body input[type=checkbox][class=checkbox]").prop("checked",false);                    
+    }
+                                                                                               
+  });    
+                
+</script>                                                                      
 </html>
